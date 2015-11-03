8c56dc5a73c3a76c4c13335bd48832a2f3250325
Marc Chapeau <marcchapeau@gmail.com>
2015-11-03 14:42:48 +0100

    new number version for intra42 package

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'chap:accounts-intra42',
-  version: '1.0.2',
+  version: '1.1.0',
   summary: 'Login service for Intranet 42 accounts',
   git: 'https://github.com/marcchapeau/meteor-accounts-intra42'
 });
@@ -10,7 +10,7 @@ Package.onUse(function (api) {
   api.use('accounts-base', ['client', 'server']);
   api.imply('accounts-base', ['client', 'server']);
   api.use('accounts-oauth', ['client', 'server']);
-  api.use('chap:intra42@1.0.0', ['client', 'server']);
+  api.use('chap:intra42@1.1.0', ['client', 'server']);
 
   api.addFiles('intra42_login_button.css', 'client');
 
